206674252d9aad49109068713b0690d5a1722756
Santiago Martí <admin@example.com>
2018-08-09 16:58:44 +0000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module AgunsaOCS
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.i18n.available_locales = :es
+    config.i18n.available_locales = [:es, :en]
     config.i18n.default_locale = :es
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
